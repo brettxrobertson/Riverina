55e8cf467539d04373ae458ab4a800d0b730a3c7
Brett <brettxrobertson@gmail.com>
2016-05-05 11:13:43 +1000

    Added customer controller

---
+++ src/main/java/Model/Customer.java
@@ -0,0 +1,106 @@
+package Model;
+
+public class Customer {
+
+	private String name;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String suburb;
+	private String state;
+	private String postcode;
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", address1=" + address1 + ", address2=" + address2 + ", address3=" + address3
+				+ ", suburb=" + suburb + ", state=" + state + ", postcode=" + postcode + "]";
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address1
+	 */
+	public String getAddress1() {
+		return address1;
+	}
+	/**
+	 * @param address1 the address1 to set
+	 */
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	/**
+	 * @return the address2
+	 */
+	public String getAddress2() {
+		return address2;
+	}
+	/**
+	 * @param address2 the address2 to set
+	 */
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	/**
+	 * @return the address3
+	 */
+	public String getAddress3() {
+		return address3;
+	}
+	/**
+	 * @param address3 the address3 to set
+	 */
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+	/**
+	 * @return the suburb
+	 */
+	public String getSuburb() {
+		return suburb;
+	}
+	/**
+	 * @param suburb the suburb to set
+	 */
+	public void setSuburb(String suburb) {
+		this.suburb = suburb;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the postcode
+	 */
+	public String getPostcode() {
+		return postcode;
+	}
+	/**
+	 * @param postcode the postcode to set
+	 */
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+}
