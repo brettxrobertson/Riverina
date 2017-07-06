bb15baf651a6959e1d0b2a80097c597c7e395fdc
Brett <brettxrobertson@gmail.com>
2017-07-06 22:51:51 +1000

    Fix controllers issue

--- src/main/java/model/Sql2oCustomer.java
+++ src/main/java/model/Sql2oCustomer.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -15,40 +16,37 @@ public class Sql2oCustomer implements CustomerController {
 	public Sql2oCustomer(Sql2o sql2o) {
 		this.sql2o = sql2o;
 	}
-	
-	@Override
-	public String addCustomer(Request req) {
-		
-		String sql = "insert into customers (name, address1, address2,"
-				+ " address3, suburb, state, postcode) values (:name, :address1,"
-				+ ":address2, :address3, :suburb, :state, :postcode)";
-		try (Connection con = sql2o.open()){
-			return con.createQuery(sql, true).throwOnMappingFailure(false)
-					.addParameter("name", req.queryParams("name"))
-					.addParameter("address1", req.queryParams("address1"))
-					.addParameter("address2", req.queryParams("address2"))
-					.addParameter("address3", req.queryParams("address3"))
-					.addParameter("suburb", req.queryParams("suburb"))
-					.addParameter("state", req.queryParams("state"))
-					.addParameter("postcode", req.queryParams("postcode"))
-					.executeUpdate()
-					.getKey().toString();
-		}catch(IllegalArgumentException e){
-			return "Wrong parameters";
-		}
-	}
 
 	@Override
 	public List<Customer> getAllCustomers() {
-
-		String sql = "select * from customers";
+		String sql = "SELECT * from customers";
 
 		try (Connection con = sql2o.open()) {
-			return con.createQuery(sql).throwOnMappingFailure(false).executeAndFetch(Customer.class);
-		} catch (Exception e) {
-			return null;
 
+			return con.createQuery(sql).setAutoDeriveColumnNames(true).executeAndFetch(Customer.class);
 		}
 	}
+
+	@Override
+	public void addCustomer(Map<String, String> params) {
+		String sql = "insert into customers (name,address1,address2,address3,state,suburb,postcode) "
+				+ "values(:name,:address1,:address2,:address3,:state,:suburb,:postcode)";
+		
+		try (Connection con = sql2o.open()) {
+		
+			con.createQuery(sql)
+				.addParameter("name", params.get("name"))
+				.addParameter("address1", params.get("address1"))
+				.addParameter("address2", params.get("address2"))
+				.addParameter("address3", params.get("address3"))
+				.addParameter("state", params.get("state"))
+				.addParameter("suburb", params.get("suburb"))
+				.addParameter("postcode", params.get("postcode"))
+				.executeUpdate();
+		}
+		
+	}
+	
+	
 
 }
